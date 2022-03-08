f54ef446a117b904f1f8fc211e52b30520cd2ac0
Christopher MCAdams <mca.christopher@gmail.com>
2022-03-07 21:31:30 -0500

    update restart method

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ def main(screen):
     game = Checkers(screen)
 
     def restart_game():
-        curses.beep()
-        messngr.send('CLEAR')
-        Checkers(screen)
+        messngr.send('Key `r` - does nothing')
         time.sleep(1)
+        messngr.send('use `q` to quit game and then start a new game')
+        time.sleep(2)
+        messngr.send('sorry')
+        time.sleep(2)
         messngr.clear()
 
     def move_cursor(c):
@@ -37,15 +39,7 @@ def main(screen):
         if c == ord('q'):
             game.state.set('isRunning', False)
         elif c == ord('r'):
-            None
-            messngr.send('Key `r` - does nothing')
-            time.sleep(1)
-            messngr.send('use `q` to quit game and then start a new game')
-            time.sleep(2)
-            messngr.send('sorry')
-            time.sleep(2)
-            messngr.clear()
-            # restart_game()
+            restart_game()
         elif c == ord('?'):
             messngr.help()
         elif c == curses.KEY_ENTER or c == 10:  # or c == 13:
